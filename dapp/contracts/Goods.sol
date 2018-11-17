b7b3facae967d38130a7fe8e42e06e437ad572f2
Srimathi Harinarayanan <srimathih@sahajsoft.com>
2018-11-17 08:14:00 +0530

    Adds contract to create a batch of goods on the smart contract

--- dapp/contracts/Goods.sol
+++ dapp/contracts/Goods.sol
@@ -1,7 +1,35 @@
 pragma solidity ^0.4.17;
 
 contract Goods {
-	function getHello() public returns(string){
+    
+    function getHello() public returns(string) {
 		return "Hello from Goods";
 	}
+
+    event BatchCreate(address sender, string uuid, string manufacturer);
+    event BatchCreateError(address sender, string uuid, string message);
+  
+    struct Batch {
+        string name;
+        string manufacturerName;
+        bool readyToShip; 
+    }
+
+    mapping(string  => Batch) private batchStore;
+
+    mapping(address => mapping(string => bool)) private batchCurrentOwner;
+
+    function createBatch(string name, string uuid, string manufacturerName) {
+    if(batchStore[uuid].readyToShip) {
+            BatchCreateError(msg.sender, uuid, "Batch with this UUID already created.");
+            return;
+        }
+        batchStore[uuid] = Batch(name, manufacturerName, true);
+        batchCurrentOwner[msg.sender][uuid] = true;
+        BatchCreate(msg.sender, uuid, manufacturerName);
+    }
+
+    function getBatchById(string uuid) constant returns (string, string) {
+        return (batchStore[uuid].name, batchStore[uuid].manufacturerName);
+    }
 }
--- dapp/test/GoodsTest.js
+++ dapp/test/GoodsTest.js
@@ -13,4 +13,14 @@ contract('Goods', () => {
         const output = await goodsInstance.getHello.call();
         assert.equal(output, 'Hello from Goods');
     });
+
+    it('should create a new batch of goods', async () => {
+        await goodsInstance.createBatch("Batch1","ABC", "Merck");
+        const goods = await goodsInstance.getBatchById('ABC');
+        assert.ok(goods);
+        assert.equal(goods[0], 'Batch1');
+    });
+
+    it('should not create new batch with existing batch ids', async () => {});
+
 })
