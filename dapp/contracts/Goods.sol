9df59b9f87ab96bf44da0e529d21e275e2915b88
Srimathi Harinarayanan <srimathih@sahajsoft.com>
2018-11-17 23:00:50 +0530

    Adds transferring goods from one account to another

--- dapp/contracts/Goods.sol
+++ dapp/contracts/Goods.sol
@@ -8,7 +8,9 @@ contract Goods {
 
     event BatchCreate(address sender, string uuid, string manufacturer);
     event BatchCreateError(address sender, string uuid, string message);
-  
+    event BatchTransfer(address sender, address receiver, string uuid);
+    event BatchTransferError(address sender, address receiver, string uuid, string message);
+
     struct Batch {
         string name;
         string manufacturerName;
@@ -31,5 +33,26 @@ contract Goods {
 
     function getBatchById(string uuid) constant returns (string, string) {
         return (batchStore[uuid].name, batchStore[uuid].manufacturerName);
+    }
+
+    function transferBatch(address to, string uuid) {
+        if(!batchStore[uuid].readyToShip) {
+            BatchTransferError(msg.sender, to, uuid, "No batch with this UUID exists");
+            return;
+        }
+        if(!batchCurrentOwner[msg.sender][uuid]) {
+            BatchTransferError(msg.sender, to, uuid, "Sender does not own this batch.");
+            return;
+        }
+        batchCurrentOwner[msg.sender][uuid] = false;
+        batchCurrentOwner[to][uuid] = true;
+        BatchTransfer(msg.sender, to, uuid);
+    }
+
+    function isCurrentOwnerOf(address owner, string uuid) constant returns (bool) {
+        if(batchCurrentOwner[owner][uuid]) {
+            return true;
+        }
+        return false;
     }
 }
--- dapp/test/GoodsTest.js
+++ dapp/test/GoodsTest.js
@@ -7,7 +7,10 @@ before(async () => {
     console.log(goodsInstance.address);
 });
 
-contract('Goods', () => {
+contract('Goods', (accounts) => {
+
+    var account_one = accounts[0];
+    var account_two = accounts[1];
 
     it('should return hello', async () => {
         const output = await goodsInstance.getHello.call();
@@ -15,12 +18,35 @@ contract('Goods', () => {
     });
 
     it('should create a new batch of goods', async () => {
-        await goodsInstance.createBatch("Batch1","ABC", "Merck");
+        await goodsInstance.createBatch("Batch1","ABC", "Merck",{from: account_one});
         const goods = await goodsInstance.getBatchById('ABC');
         assert.ok(goods);
         assert.equal(goods[0], 'Batch1');
+        const isOwnerResultAcct1 = await goodsInstance.isCurrentOwnerOf(account_one,'ABC');
+        assert.equal(isOwnerResultAcct1, true);
     });
 
-    it('should not create new batch with existing batch ids', async () => {});
+    it('should not create new batch with existing batch ids', async () => {
+        await goodsInstance.createBatch("Batch1","ABC", "Merck",{from: account_one});
+        await goodsInstance.createBatch("Batch2","ABC", "Merck1",{from: account_one});
+        // what to assert? Event check?
+    });
 
+    it('should transfer a batch of manafactured goods to another', async () => {
+      await goodsInstance.createBatch("Batch2","BCD", "Merck",{from: account_one});
+      const goods = await goodsInstance.getBatchById('BCD');
+      assert.ok(goods);
+      assert.equal(goods[0], 'Batch2');
+      await goodsInstance.transferBatch(account_two, "BCD", {from:account_one});
+      const isOwnerResultAcct1 = await goodsInstance.isCurrentOwnerOf(account_one,'BCD');
+      const isOwnerResultAcct2 = await goodsInstance.isCurrentOwnerOf(account_two,'BCD');
+      assert.equal(isOwnerResultAcct1, false);
+      assert.equal(isOwnerResultAcct2, true);
+
+    });
+
+    it('should not transfer batch without createBatch step', async () => {
+        await goodsInstance.transferBatch(account_two, "DEF", {from:account_one});
+        //what to assert? Event check?
+    });
 })
