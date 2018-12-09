cb9fa061af5f9c86284d86265ed6b76c1feb778a
Vignesh Ravichandran <vigneshr@sahajsoft.com>
2018-12-09 10:44:51 +0530

    Move js and css folders to public and make public folder act as web root

--- frontend/server.js
+++ frontend/server.js
@@ -3,11 +3,7 @@ var path = require('path');
 var app = express();
 var port = 9000;
 
-app.use(express.static(__dirname));
-
-app.get('/js/index.js', function(req, res) {
-    res.sendFile(path.join(__dirname + '/js/index.js'));
-});
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
